4d6ee6b6e710a86e37c8666763519f1e33c63dcc
Pierre Konanz <pierre.konanz@gmail.com>
2024-03-17 01:07:51 +0100

    recup css et ref dans modal

--- wp-content/themes/motaphoto/assets/js/app.js
+++ wp-content/themes/motaphoto/assets/js/app.js
@@ -19,6 +19,28 @@ document.addEventListener("DOMContentLoaded", function() {
         modal.style.display = 'block';
         overlay.classList.add('active');
         placementModale.style.display = 'block'; // Affiche la modale
+
+        // Récupérer la référence de la photo
+        var referenceElement = document.querySelector('.photo-informations p:nth-child(2)');
+        if (referenceElement) {
+            var referenceText = referenceElement.textContent.trim();
+            // Diviser la chaîne en fonction de la séquence ":"
+            var referenceParts = referenceText.split(':');
+            if (referenceParts.length > 1) {
+                var reference = referenceParts[1].trim(); // Récupérer la partie après le ":"
+                // Pré-remplir le champ de référence dans le formulaire de la modale
+                var referenceInput = document.querySelector('input[name="your-subject"]');
+                if (referenceInput) {
+                    referenceInput.value = reference;
+                } else {
+                    console.error('Le champ de référence n\'a pas été trouvé.');
+                }
+            } else {
+                console.error('La référence n\'a pas été trouvée dans le format attendu.');
+            }
+        } else {
+            console.error('Élément contenant la référence non trouvé.');
+        }
     });
 
     var closeModalBtn = document.getElementById('closeModalBtn');
@@ -26,6 +48,15 @@ document.addEventListener("DOMContentLoaded", function() {
         modal.style.display = 'none';
         overlay.classList.remove('active');
         placementModale.style.display = 'none'; // Cache la modale
+    });
+
+    // Fermer la modale lorsque l'utilisateur clique en dehors de celle-ci
+    overlay.addEventListener('click', function(event) {
+        if (event.target === overlay) {
+            modal.style.display = 'none';
+            overlay.classList.remove('active');
+            placementModale.style.display = 'none'; // Cache la modale
+        }
     });
 
     // Nouvel événement de clic pour le bouton "Contact" du menu
@@ -39,4 +70,14 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+jQuery(document).ready(function($) {
+    // Lorsque l'utilisateur clique sur le bouton du menu burger
+    $('.burger-menu').click(function() {
+        // Basculez la visibilité du menu principal
+        $('#site-navigation').slideToggle();
+        // Basculez la classe .open pour changer l'apparence de l'icône
+        $(this).toggleClass('open');
+    });
+});
 
--- wp-content/themes/motaphoto/single-photo.php
+++ wp-content/themes/motaphoto/single-photo.php
@@ -12,6 +12,9 @@ while (have_posts()) : the_post();
 
     <div class="placement-fiche">
         <div class="fiche-photo">
+        <div class="photo_img_mobile inactive active-mobile">
+                <?php echo $thumbnail; ?>
+            </div>
             <div class="photo-informations">
                 <h2><?php echo get_the_title($post_id); ?></h2>
                 <p>référence : <?php echo get_post_meta($post_id, 'reference', true) ? get_post_meta($post_id, 'reference', true) : 'Aucune référence définie pour cet article.'; ?></p>
@@ -74,41 +77,46 @@ while (have_posts()) : the_post();
         </div>
     </div>
 
-    <div class="presentation-autres-photos">
-        <div class="presentation-texte">
-            <p>VOUS AIMEREZ AUSSI</p>
-        </div>
-        <div class="presentation-images">
-            <?php 
-                $args = array(
-                    'post_type' => 'photo', // Le type de publication personnalisé
-                    'posts_per_page' => 2, // Récupère tous les articles de cette taxonomie
-                    'tax_query' => array(
-                        array(
-                            'taxonomy' => 'categorie', // récupérer les catégories
-                            'field' => 'slug', 
-                            'terms' => get_the_terms($post_id, 'categorie') ? get_the_terms($post_id, 'categorie')[0]->slug : '', // on veut des images qui ont la même catégorie que notre image en cours
-                        ),
-                    ),
-                );
+    <div class="presentation-images">
+    <?php 
+    $args = array(
+        'post_type' => 'photo', // Le type de publication personnalisé
+        'posts_per_page' => 2, // Récupère tous les articles de cette taxonomie
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'categorie', // récupérer les catégories
+                'field' => 'slug', 
+                'terms' => get_the_terms($post_id, 'categorie') ? get_the_terms($post_id, 'categorie')[0]->slug : '', // on veut des images qui ont la même catégorie que notre image en cours
+            ),
+        ),
+    );
 
-                $query = new WP_Query($args);
+    $query = new WP_Query($args);
 
-                if ($query->have_posts()) {
-                    while ($query->have_posts()) {
-                        $query->the_post();
-                        $urlrelated = get_the_permalink();
-                        echo("<a href='".$urlrelated."' class='presentation-images-gauche'><div >");
-                            // Ajoutez une classe CSS à la balise <img> pour spécifier une largeur maximale
-                            the_post_thumbnail('post-thumbnail', array('class' => 'custom-max-width-image'));
-                            // Le contenu de chaque article ici.
-                        echo("</div></a>");
-                    }
-                    wp_reset_postdata(); // Réinitialise la requête WP_Query.
-                }
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $urlrelated = get_the_permalink();
             ?>
-        </div>
-        
+            <a href="<?php echo $urlrelated; ?>" class="presentation-images-gauche">
+                <div class="image-container">
+                    <?php the_post_thumbnail('post-thumbnail', array('class' => 'custom-max-width-image')); ?>
+                </div>
+                <div class="overlay">
+                    <img class="icon-fullscreen" src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_fullscreen.png" alt="Icon fullscreen">
+                    <img class="icon-eye" src="<?php echo get_template_directory_uri(); ?>/assets/images/eye-icon.png" alt="Eye Icon">
+                    <div class="info-photo">
+                        <p>Référence : <?php echo get_post_meta(get_the_ID(), 'reference', true); ?></p>
+                        <p>Catégorie : <?php echo get_the_terms(get_the_ID(), 'categorie') ? get_the_terms(get_the_ID(), 'categorie')[0]->name : 'Aucune catégorie définie pour cet article.'; ?></p>
+                    </div>
+                </div>
+            </a>
+            <?php
+        }
+        wp_reset_postdata(); // Réinitialise la requête WP_Query.
+    }
+    ?>
+</div>
     </div>
 
 <?php
